104f3e3fd730a1170eaadd245f8d9aff147d308a
dhrupal0033 <110047597+dhrupal0033@users.noreply.github.com>
2024-09-30 17:47:28 +0530

    Update index.js

--- src/components/molecules/Banner/index.js
+++ src/components/molecules/Banner/index.js
@@ -36,11 +36,11 @@ const Banner = () => {
             className="text-2xl text-primary font-bold translate-y-[-80%] sm:translate-y-[-0%]"
             cursor={true}
             sequence={[
-              "A Mern-stack Developer",
-              2000,
               "A Full-stack Developer",
               2000,
-              "A Front-end Developer",
+              "A SEO Specialist",
+              2000,
+              "A Mern-stack Developer",
               2000,
             ]}
             wrapper="div"
