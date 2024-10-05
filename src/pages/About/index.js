c7ab025dab368c71663c4748eae897bfb5428290
dhrupal0033 <110047597+dhrupal0033@users.noreply.github.com>
2024-10-05 11:28:49 +0530

    Update index.js about

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -106,7 +106,7 @@ const About = () => {
                 Gujarat, India
               </h2>
               <a
-                href="https://drive.google.com/file/d/19rnbukAhf9oPhadMhsvI3xnWF6FIYeMT/view?usp=share_link"
+                href="https://drive.google.com/file/d/1J_03UybqT3CATJI9ZbvW05EYWFdcGSpl/view?usp=drive_link"
                 target="blank"
               >
                 <div className="flex justify-center md:justify-start translate-y-[60%] sm:translate-y-[-0%]">
